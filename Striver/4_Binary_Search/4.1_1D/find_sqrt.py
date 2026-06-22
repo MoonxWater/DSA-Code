a3e5720316d26f3a416b99851155b979db4c3bef
Moon Water <moonxwaterx@gmail.com>
2026-06-23 01:28:56 +0530

    find sqrt sol 1

---
+++ Striver/4_Binary_Search/4.1_1D/find_sqrt.py
@@ -0,0 +1,46 @@
+from engine import Engine
+
+test_cases = [([28], {'ret': 5}), 
+              ([25], {'ret': 5}),
+              ([89], {'ret': 9})]
+run = Engine(test_cases)
+
+
+'''
+About this problem: Find the sqrt of the given number in O(logn)
+'''
+
+
+#---Solution-----------------------------------------------------------------------------
+
+'''
+low to 1 and high to the given number
+while low <= high
+calc mid of low and high
+calc mid * mid
+if midsq is greater than number, eliminate the right half
+if midsq is smaller than the number, eliminate the left half and store it as possible answer
+'''
+
+def solution(n: int) -> int:
+    low, high = 1, n
+    ans = 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        midsq = mid * mid
+
+        if midsq == n:
+            return mid
+        
+        elif midsq > n:
+            high = mid - 1
+        
+        else:
+            ans = mid
+            low = mid + 1
+
+    return ans
+
+run.v8(solution)
